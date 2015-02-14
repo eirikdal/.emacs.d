b57394f98a27ef430bc7bfc5b001da8bc091e9a7
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2015-02-14 09:40:15 +0100

    Fixed some problems with new setups..

--- sane-defaults.el
+++ sane-defaults.el
@@ -52,10 +52,6 @@
 ;; Lines should be 80 characters wide, not 72
 (setq fill-column 80)
 
-;; Save a list of recent files visited. (open recent file with C-x f)
-(recentf-mode 1)
-(setq recentf-max-saved-items 100) ;; just 20 is too recent
-
 ;; Save minibuffer history
 (savehist-mode 1)
 (setq history-length 1000)
