479d844dc213b93ef1f4861d2595111193490e77
Eirik Daleng Haukedal <eirik.haukedal@gmail.com>
2015-02-15 11:46:00 +0100

    Keybindings and gitingore

--- key-bindings.el
+++ key-bindings.el
@@ -1,6 +1,6 @@
 ;; I don't need to kill emacs that easily
 ;; the mnemonic is C-x REALLY QUIT
-(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'save-buffers-kill-terminal)
 (global-set-key (kbd "C-x C-c") 'delete-frame)
 
 ;; Completion that uses many different methods to find options.
@@ -21,7 +21,7 @@
 
 ;; Expand region (increases selected region by semantic units)
 (global-set-key (if is-mac (kbd "C-@") (kbd "C-'")) 'er/expand-region)
-(global-set-key (if is-mac (kbd "C-@") (kbd "M-'")) 'er/contract-region)
+(global-set-key (if is-mac (kbd "C-@") (kbd "C-M-'")) 'er/contract-region)
 
 ;; Experimental multiple-cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -226,7 +226,7 @@
 (global-set-key (kbd "<XF86WakeUp>") 'beginning-of-buffer)
 
 ;; Query replace regex key binding
-(global-set-key (kbd "M-&") 'query-replace-regexp)
+(global-set-key (kbd "M-s-5") 'query-replace-regexp)
 
 ;; Yank selection in isearch
 (define-key isearch-mode-map (kbd "C-o") 'isearch-yank-selection)
